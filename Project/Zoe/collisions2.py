28420432dc82670e33fefa5ff93b6b2a0f1778a8
zkaiser3 <zkaiser3@illinois.edu>
2024-03-24 12:49:14 -0500

    update collisions

--- Project/Zoe/collisions2.py
+++ Project/Zoe/collisions2.py
@@ -2,10 +2,10 @@ import pygame, sys, time
 
 #defining classes
 class StaticObstacle(pygame.sprite.Sprite):
-    def __init__(self,pos,size,groups):
+    def __init__(self,pos,size,groups,color):
         super().__init__(groups)
         self.image = pygame.Surface(size)
-        self.image.fill('yellow')
+        self.image.fill(color)
         self.rect = self.image.get_rect(topleft = pos)
         self.old_rect = self.rect.copy()
 
@@ -17,7 +17,7 @@ class Player(pygame.sprite.Sprite):
         self.image.fill('blue')
 
         #position
-        self.rect = self.image.get_rect(topleft = (640,360))
+        self.rect = self.image.get_rect(topleft = (200,200))
         self.old_rect = self.rect.copy()
 
         #movement
@@ -29,15 +29,15 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
 
         #movement input
-        if keys[pygame.K_w]:
-            self.direction.y = -1
-        elif keys[pygame.K_s]:
+        if keys[pygame.K_s]:
             self.direction.y = 1
+        elif keys[pygame.K_w]:
+            self.direction.y = -1
         else:
             self.direction.y = 0
 
         if keys[pygame.K_d]:
-            self.direction.x = -1
+            self.direction.x = 1
         elif keys[pygame.K_a]:
             self.direction.x = -1
         else:
@@ -50,9 +50,12 @@ class Player(pygame.sprite.Sprite):
             self.input()
 
             if self.direction.magnitude() != 0:
-                self.direction = self.direction.normalize()
+                self.direction.x = self.direction.x.normalize()
+                self.direction.y = self.direction.y.normalize()
 
-            self.pos += self.direction * self.speed * dt
+            self.pos.x += self.direction.x * self.speed.x * dt
+            self.pos.y += self.direction.y * self.speed.y * dt
+
             self.rect.topleft = round(self.pos.x), round(self.pos.y)
 
 class Ball(pygame.sprite.Sprite):
@@ -67,6 +70,8 @@ class Ball(pygame.sprite.Sprite):
         self.speed = 400
         self.old_rect = self.rect.copy()
 
+pygame.init()
+
 #general setup
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 400
@@ -77,11 +82,11 @@ all_sprites = pygame.sprite.Group()
 collision_sprites = pygame.sprite.Group()
 
 #sprite setup
-StaticObstacle((100,300),(100,50),[all_sprites,collision_sprites])
-StaticObstacle((800,600),(100,200),[all_sprites,collision_sprites])
-StaticObstacle((900,200),(200,10),[all_sprites,collision_sprites])
-Player(all_sprites)
-Ball(all_sprites)
+StaticObstacle((100,300),(100,50),[all_sprites,collision_sprites],'yellow')
+StaticObstacle((400,100),(100,100),[all_sprites,collision_sprites],'blue')
+StaticObstacle((250,200),(100,100),[all_sprites,collision_sprites],'green')
+player = Player(all_sprites)
+ball = Ball(all_sprites)
 
 #loop
 last_time = time.time()
@@ -97,10 +102,12 @@ while True:
             pygame.quit()
             sys.exit()
 
+    for sprite in all_sprites.sprites():
+        pygame.draw.rect(screen,'orange',sprite.old_rect)
+
     #drawing and updating the screen
     screen.fill('black')
     all_sprites.update(dt)
-
     all_sprites.draw(screen)
 
     pygame.display.update() 
